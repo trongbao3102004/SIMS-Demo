2ba598e06f4de552a22d8353b080bbfa4a53a1dd
LeNgocHung114 <hungcute1142004@gmail.com>
2024-04-11 23:46:21 +0700

    SIMS

--- SIMS-Demo/Controllers/AuthenticationController.cs
+++ SIMS-Demo/Controllers/AuthenticationController.cs
@@ -8,23 +8,37 @@ namespace SIMS_Demo.Controllers
 	public class AuthenticationController : Controller
 	{
 
-		[HttpPost]
-		public IActionResult Login (User user)
-		{
-			List<User>? users = LoadUsersFromFile("users.json");
-			var result = users.FirstOrDefault(u => u.UserName == user.UserName && u.Pass == user.Pass);
-			if (result != null)
-			{
-				HttpContext.Session.SetString("UserName", result.UserName);
-				HttpContext.Session.SetString("Role", result.Role);
-				return RedirectToAction("Index", "Teacher");
-			}
-			else
-			{
-				ViewBag.error = "Invalid user";
-				return View("Login");
-			}
-		}
+        [HttpPost]
+        public IActionResult Login(User user)
+        {
+            // Đọc thông tin người dùng từ file users.json
+            List<User> users = LoadUsersFromFile("users.json");
+            var result = users.Find(u => u.UserName == user.UserName && u.Pass == user.Pass);
+
+            if (result != null)
+            {
+                // Lưu thông tin người dùng vào session
+                HttpContext.Session.SetString("UserName", result.UserName);
+                HttpContext.Session.SetString("Role", result.Role);
+
+                // Chuyển hướng đến trang tương ứng với vai trò của người dùng
+                switch (result.Role)
+                {
+                    case "teacher":
+                        return RedirectToAction("Index", "Teacher");
+                    case "student":
+                        return RedirectToAction("Index", "Student");
+                    default:
+                        return RedirectToAction("Index", "Home"); // Chuyển hướng mặc định nếu không phân quyền
+                }
+            }
+            else
+            {
+                // Thông báo lỗi nếu tài khoản không hợp lệ
+                ViewBag.error = "Invalid user!";
+                return View("Login");
+            }
+        }
         [HttpGet]
         public IActionResult Login()
 		{
---
+++ SIMS-Demo/Controllers/StudentController.cs
@@ -0,0 +1,142 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SIMS_Demo.Models;
+using System.Text.Json;
+
+namespace SIMS_Demo.Controllers
+{
+    public class StudentController : Controller
+    {
+        static List<Student> students = new List<Student>();
+        public IActionResult Delete(int Id)
+        {
+            var students = LoadStudentFromFile("studentdata.json");
+            var searchStudent = students.FirstOrDefault(t => t.Id == Id);
+            students.Remove(searchStudent);
+
+            var options = new JsonSerializerOptions { WriteIndented = true };
+            string jsonString = JsonSerializer.Serialize(students, options);
+            //Save file
+            using (StreamWriter writer = new StreamWriter("studentdata.json"))
+            {
+                writer.Write(jsonString);
+            }
+            return RedirectToAction("Index");
+        }
+        [HttpPost]
+        public IActionResult NewStudent(Student student)
+        {
+            students.Add(student);
+            var option = new JsonSerializerOptions { WriteIndented = true };
+            string jsonString = JsonSerializer.Serialize(students, option);
+
+            using (StreamWriter writer = new StreamWriter("studentdata.json"))
+            {
+                writer.Write(jsonString);
+            }
+            return Content(jsonString);
+        }
+ 
+        [HttpGet]
+        public IActionResult Save()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult NewStudent()
+        {
+            return View();
+        }
+        public List<Student>? LoadStudentFromFile(string filename)
+        {
+            string readText = System.IO.File.ReadAllText("studentdata.json");
+            return JsonSerializer.Deserialize<List<Student>>(readText);
+        }
+        public ActionResult Index()
+        {
+            ViewBag.UserName = HttpContext.Session.GetString("UserName");
+            ViewBag.Role = HttpContext.Session.GetString("Role");
+            students = LoadStudentFromFile("studentdata.json");
+            return View(students);
+        }
+
+        // GET: StudentController/Details/5
+        public ActionResult ManageStudent(int id)
+        {
+            ViewBag.UserName = HttpContext.Session.GetString("UserName");
+            ViewBag.Role = HttpContext.Session.GetString("Role");
+            students = LoadStudentFromFile("student.json");
+            return View();
+        }
+
+        // GET: StudentController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: StudentController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: StudentController/Edit/5
+        //public ActionResult Edit(int id)
+        //{
+        //    return View();
+        //}
+
+        // POST: TeacherController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Student student)
+        {
+            var existingStudent = students.FirstOrDefault(t => t.Id == student.Id);
+            if (existingStudent == null)
+            {
+                return NotFound();
+            }
+            existingStudent.Id = student.Id;
+            existingStudent.Name = student.Name;
+            existingStudent.DoB = student.DoB;
+            var options = new JsonSerializerOptions { WriteIndented = true };
+            string jsonString = JsonSerializer.Serialize(students, options);
+            System.IO.File.WriteAllText("studentdata.json", jsonString);
+            return RedirectToAction("Index");
+        }
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+
+            {
+                return View();
+            }
+        }
+        // POST: StudentController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+
+    }
+}
